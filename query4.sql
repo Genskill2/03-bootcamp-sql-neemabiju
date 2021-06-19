03d9e13814acd42c97cc780d6c86d9a5edc40f0d
neemabiju <neemabiju1@gmail.com>
2021-06-19 18:38:52 +0530

    added assignment

---
+++ query4.sql
@@ -0,0 +1,1 @@
+select name from subjects,books_subjects  where subjects.id=books_subjects.subject and books_subjects.book=6; 
---
+++ update1.sql
@@ -0,0 +1,2 @@
+UPDATE publisher set name="Prentice Hall" where name="PHI";
+DELETE subject FROM books_subjects where id=8;
