3f62ec5847ac0282154252b5221bf66717f7b732
neemabiju <neemabiju1@gmail.com>
2021-06-19 19:11:42 +0530

    added assignment

---
+++ create.sql
@@ -0,0 +1,4 @@
+CREATE TABLE publisher(id INTEGER PRIMARY KEY,name VARCHAR(250),country VARCHAR(250));
+CREATE TABLE books(id INTEGER PRIMARY KEY,title VARCHAR(250),publisher INTEGER,FOREIGN KEY(publisher)REFERENCES publisher(id));
+CREATE TABLE subjects(id INTEGER PRIMARY KEY,name VARCHAR(250));
+CREATE TABLE books_subjects(book INTEGER,subject INTEGER,FOREIGN KEY(book)REFERENCES books(id),FOREIGN KEY(subject)REFERENCES subjects(id));
---
+++ query1.sql
@@ -0,0 +1,1 @@
+select b.title from books b, publisher a where b.publisher=a.id and a.name="PHI";
